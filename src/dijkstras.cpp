202080ed54fc5ccf27711f9bf393e7514c3d268f
Aarush <aarushinamdar@gmail.com>
2025-03-16 21:48:25 -0700

    fixed print in dijk

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -39,7 +39,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 }
 
 void print_path(const vector<int>& path, int total) {
-    for (size_t i = 0; i < path.size(); ++i)  cout << path[i];
+    for (size_t i = 0; i < path.size(); ++i)  cout << path[i] << " ";
     cout << endl;
     cout << "Total cost is " << total << endl;
 }
